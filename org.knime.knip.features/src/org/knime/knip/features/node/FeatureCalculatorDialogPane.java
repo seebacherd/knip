789a95b9a116c2684964599148ad26d7ecef1793
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2016-01-16 15:59:18 +0100

    Add OverlappingLabelFilter.

--- org.knime.knip.features/src/org/knime/knip/features/node/FeatureCalculatorDialogPane.java
+++ org.knime.knip.features/src/org/knime/knip/features/node/FeatureCalculatorDialogPane.java
@@ -111,6 +111,7 @@ public class FeatureCalculatorDialogPane<T extends RealType<T> & NativeType<T>, 
 	private DialogComponentBoolean intersectionModeComponent;
 	private DialogComponentBoolean appendSegmentInformationComponent;
 	private DialogComponentFilterSelection<L> includeLabelsComponent;
+	private DialogComponentFilterSelection<L> filterOverlappingLabelsComponent;
 
 	/**
 	 * Feature Set GUI Elements
@@ -143,7 +144,7 @@ public class FeatureCalculatorDialogPane<T extends RealType<T> & NativeType<T>, 
 
 		// create label settings panel
 		this.labelSettingsPanel = new LabelSettingsPanel(appendLabelsOfOverlappingSegments, intersectionModeComponent,
-				appendSegmentInformationComponent, includeLabelsComponent);
+				appendSegmentInformationComponent, includeLabelsComponent, filterOverlappingLabelsComponent);
 
 		// arrange all components
 		this.featureSetConfigPanel = new FeatureSetConfigPanel(columnSelectionPanel, dimensionSelectionPanel,
@@ -151,7 +152,7 @@ public class FeatureCalculatorDialogPane<T extends RealType<T> & NativeType<T>, 
 
 		this.addTab("Feature Set Configuration", this.featureSetConfigPanel);
 
-		this.addTab("Label Segment Settings", this.labelSettingsPanel);
+		this.addTab("Segment Settings", this.labelSettingsPanel);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -191,6 +192,10 @@ public class FeatureCalculatorDialogPane<T extends RealType<T> & NativeType<T>, 
 		this.includeLabelsComponent = new DialogComponentFilterSelection<L>(
 				FeatureCalculatorModel.<L> createIncludeLabelModel());
 
+		// filter overlapping labels settings
+		this.filterOverlappingLabelsComponent = new DialogComponentFilterSelection<L>(
+				FeatureCalculatorModel.<L> createFilterOverlappingLabelModel());
+		
 		// feature set selection
 		this.smfs = FeatureCalculatorModel.createFeatureSetsModel();
 	}
